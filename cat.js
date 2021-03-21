d79a5b849a5c391ab3bafdcf5b39594a5e7ec9f4
schrodinger2 <schrodinger2@users.noreply.github.com>
2021-03-22 00:00:51 +0200

    changing color per time unit

--- cat.js
+++ cat.js
@@ -1,9 +1,9 @@
 let targetstring = "i am a cat";
 let string = "";
+
 // there must be a better way...but i will go with this
 const alphapet = ["q","w", "e", "r", "t","y", "u", "i","o", "p", "a",
 "s", "d", "f", "g", "h", "j", "k", "l", "z", "x","c","v", "b", "n", "m", " "];
-
 
 function randomizer() {
    let random = Math.floor(Math.random()*27);
@@ -16,15 +16,7 @@ for (var i = 0; i < targetstring.length; i++) {
 }
 document.querySelector(".para").textContent = string;
 
-// tried and didnot work
-        // for (var i = 0; i < targetstring.length; i++) {
-        //   for (var j = string[i]; j != targetstring[i]; j = randomizer()) {
-        //       string[i] = randomizer();
-        //       console.log(string);
-        //
-        //   }
-          // while (string != targetstring) {)
-
+//changing the string letters randomly
 
 function thisShouldHaveBeenAClass(i) {
 
@@ -33,18 +25,23 @@ function thisShouldHaveBeenAClass(i) {
       // cant use string.replace because u cant specify which occurrence should be replaced
       string = randomReplacer(string, i)
       document.querySelector(".para").textContent = string;
-      console.log(string);
       if (string.charAt(i) == targetstring.charAt(i)) {clearInterval(changer);}
     }, 100);
-
 }
 
-for (var i = 0; i < targetstring.length; i++) {
-  thisShouldHaveBeenAClass(i)
-}
+let startingColor = 100 //rgb
+setInterval( () => { timeGradient(2); }, 100);
 
+for (var i = 0; i < targetstring.length; i++) {thisShouldHaveBeenAClass(i)}
 
 function randomReplacer(string, i) {
   let str = string.slice(0, i) + randomizer() + string.slice(i + 1) //deleting the (i)th letter from the string and adding random one
   return str;
+}
+
+
+function timeGradient(i) {
+  startingColor += i
+  document.querySelector(".para").style.color = `rgb(${startingColor} ,${startingColor} ,${startingColor})`;
+
 }
